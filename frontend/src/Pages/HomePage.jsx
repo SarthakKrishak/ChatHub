f54cab4cdd8989e87031a02cde4491147c81238e
Sarthak <sarthakkrishak1234@gmail.com>
2025-06-13 00:01:32 +0530

    height fix

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -8,7 +8,7 @@ const HomePage = () => {
   const { selectedUser } = useChatStore();
 
   return (
-    <div className="h-full w-full bg-base-200">
+    <div className="min-h-screen w-full bg-base-200">
       <div className="flex items-center justify-center pt-20 px-10 bg-red">
         <div className="bg-base-100 rounded-lg shadow-cl w-full  h-[calc(100vh-8rem)]">
           <div className="flex h-full rounded-lg overflow-hidden ">
